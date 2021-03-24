4547d8a2c415e82fc88691c4ce6db68836b540db
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-24 16:16:27 -0400

    add transfer bank calc

--- js/banking-in-javascript.js
+++ js/banking-in-javascript.js
@@ -46,5 +46,19 @@ class Savings {
 
 }
 
-let s1 = new Savings(1, "Sav1")
+let s1 = new Savings(1, "Sav1");
+s1.deposit(2000);
+s1.withdraw(700);
+console.log("S1 bal is", s1.bal);
 
+let s2 = new Savings(2, "Sav2");
+s2.deposit(200);
+s2.withdraw(40);
+console.log("S2 bal is", s2.bal);
+
+s1.transfer(100, s2);
+console.log("S1 bal is", s1.bal);
+console.log("S2 bal is", s2.bal);
+
+s1.deposit(-1);
+s2.withdraw(10000);
